b0f00401d0e872e2e7a0d936a8a6c4c8cdb8eb86
Iker <iker_izagirre@hotmail.com>
2016-10-13 07:24:43 +0000

    13/10/2016-9:24

--- PHP-Iker/PHP-ToDo-App/todo.sql
+++ PHP-Iker/PHP-ToDo-App/todo.sql
@@ -26,7 +26,7 @@ CREATE TABLE `listas` (
   `ID` int(8) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(25) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,6 +35,7 @@ CREATE TABLE `listas` (
 
 LOCK TABLES `listas` WRITE;
 /*!40000 ALTER TABLE `listas` DISABLE KEYS */;
+INSERT INTO `listas` VALUES (1,'Egiteko'),(2,'Egiten'),(3,'Eginda'),(4,'Egiteko2'),(5,'Egiteko3'),(6,'Eginda2');
 /*!40000 ALTER TABLE `listas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,11 +51,12 @@ CREATE TABLE `tareas` (
   `nombre` varchar(30) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
   `hecho` tinyint(1) DEFAULT '0',
+  `fechaFin` date NOT NULL,
   `ID_listas` int(8) unsigned NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `ID_listas` (`ID_listas`),
   CONSTRAINT `tareas_ibfk_1` FOREIGN KEY (`ID_listas`) REFERENCES `listas` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,6 +65,7 @@ CREATE TABLE `tareas` (
 
 LOCK TABLES `tareas` WRITE;
 /*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
+INSERT INTO `tareas` VALUES (1,'Bazkaria','Makarroiak',0,'0000-00-00',1),(2,'Korri itea','Orduerditxo bat',0,'0000-00-00',2),(4,'Kaka egin','popo',0,'0000-00-00',4),(5,'Tele ikusi','Salvame limon',0,'0000-00-00',1),(6,'Jolastu','Visio Bisio',0,'0000-00-00',5);
 /*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +85,7 @@ CREATE TABLE `usuarios` (
   `telefono` varchar(30) DEFAULT NULL,
   `email` varchar(40) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +94,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Izayer','iker','Iker','Izaguirre','943555677','iker_izaguirre@hotmail.com'),(2,'Ravasaurio','aitor','Aitor','Redondo','943555678','aredondob@gmail.com'),(4,'asdf','asdf','asdf','',NULL,'asdf'),(5,'Patata','haizea','Haizea','',NULL,'haizea@gmail.com'),(6,'xempro','asdf','Xempronio','',NULL,'adf@asdf'),(7,'eloisa','asdf','eloisa','',NULL,'eloisa@asdf'),(8,'po','asdf','asdf','',NULL,'af@asdf');
+INSERT INTO `usuarios` VALUES (1,'Izayer','iker','Iker','Izaguirre','943555677','iker_izaguirre@hotmail.com'),(2,'Ravasaurio','aitor','Aitor','Redondo','943555678','aredondob@gmail.com'),(4,'asdf','asdf','asdf','',NULL,'asdf'),(5,'Patata','haizea','Haizea','',NULL,'haizea@gmail.com'),(6,'xempro','asdf','Xempronio','',NULL,'adf@asdf'),(7,'eloisa','asdf','eloisa','',NULL,'eloisa@asdf'),(8,'po','asdf','asdf','',NULL,'af@asdf'),(9,'miniom','asdf','miniom','',NULL,'miniom@asdf'),(10,'l','asdf','l','',NULL,'l@l'),(11,'unai','unai','unai','',NULL,'unai@unai'),(12,'ttopttoro','asdf','ttopttoro','',NULL,'ttopttoro@ttopttoro'),(13,'juan','juan','juan','',NULL,'juana@juan');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,7 +114,7 @@ CREATE TABLE `usuarios_listas` (
   KEY `ID_listas` (`ID_listas`),
   CONSTRAINT `usuarios_listas_ibfk_1` FOREIGN KEY (`ID_usuarios`) REFERENCES `usuarios` (`ID`),
   CONSTRAINT `usuarios_listas_ibfk_2` FOREIGN KEY (`ID_listas`) REFERENCES `listas` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,6 +123,7 @@ CREATE TABLE `usuarios_listas` (
 
 LOCK TABLES `usuarios_listas` WRITE;
 /*!40000 ALTER TABLE `usuarios_listas` DISABLE KEYS */;
+INSERT INTO `usuarios_listas` VALUES (1,1,1),(2,1,2),(3,1,3),(4,2,4),(5,2,5),(6,2,6);
 /*!40000 ALTER TABLE `usuarios_listas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -132,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-20  6:35:41
+-- Dump completed on 2016-10-13  7:21:26
